d98f8975a2b42f992ae47035bdea37f466e49e7a
RiccardoStud <s292520@studenti.polito.it>
2025-02-17 17:17:59 +0100

    Fix: due to visual studio code bug, this is the leftover of the precedent commit

---
+++ server_grpc/go.mod
@@ -0,0 +1,33 @@
+module server_grpc
+
+go 1.23.4
+
+require (
+	google.golang.org/grpc v1.70.0
+	google.golang.org/protobuf v1.36.5
+	k8s.io/autoscaler/cluster-autoscaler v0.0.0-20250217131422-2837c59d95be
+)
+
+require (
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/api v0.33.0-alpha.0 // indirect
+	k8s.io/apimachinery v0.33.0-alpha.0 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/utils v0.0.0-20241104100929-3ea5e8cea738 // indirect
+	sigs.k8s.io/json v0.0.0-20241010143419-9aa6b5e7a4b3 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.2 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
+)
---
+++ server_grpc/server.go
@@ -0,0 +1,264 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/anypb"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/externalgrpc/protos"
+
+	"os/exec"
+)
+
+// protos->package with the files definition (_grpc.pb.go and pb.go) born from the proto file
+type cloudProviderServer struct {
+	protos.UnimplementedCloudProviderServer
+}
+
+// Hardcoded invented type
+type GPUTypes struct {
+	Name  string
+	Specs string
+}
+
+// hardcoded flag for increase size
+var test int = 1
+
+// NodeGroups returns all node groups configured for this cloud provider.
+func (s *cloudProviderServer) NodeGroups(ctx context.Context, req *protos.NodeGroupsRequest) (*protos.NodeGroupsResponse, error) {
+	//here TODO the real computations
+	return &protos.NodeGroupsResponse{
+		NodeGroups: []*protos.NodeGroup{
+			{
+				Id:      "Sud",
+				MinSize: 1,
+				MaxSize: 3,
+			},
+		},
+	}, nil
+}
+
+// NodeGroupForNode returns the node group for the given node.
+// The node group id is an empty string if the node should not
+// be processed by cluster autoscaler.
+func (c *cloudProviderServer) NodeGroupForNode(ctx context.Context, req *protos.NodeGroupForNodeRequest) (*protos.NodeGroupForNodeResponse, error) {
+	//here TODO the real computations
+	log.Printf("NOME NODO PER CUI VUOLE SAPERE NODEGROUP %s", req.Node.Name)
+	return &protos.NodeGroupForNodeResponse{
+		NodeGroup: &protos.NodeGroup{
+			Id:      "Sud",
+			MinSize: 1,
+			MaxSize: 3,
+		},
+	}, nil
+}
+
+// PricingNodePrice returns a theoretical minimum price of running a node for
+// a given period of time on a perfectly matching machine.
+// Implementation optional: if unimplemented return error code 12 (for `Unimplemented`)
+func (c *cloudProviderServer) PricingNodePrice(ctx context.Context, req *protos.PricingNodePriceRequest) (*protos.PricingNodePriceResponse, error) {
+	//here TODO the real computations
+	return nil, status.Error(codes.Unimplemented, "function PricingNodePrice is Unimplemented")
+}
+
+// PricingPodPrice returns a theoretical minimum price of running a pod for a given
+// period of time on a perfectly matching machine.
+// Implementation optional: if unimplemented return error code 12 (for `Unimplemented`)
+func (c *cloudProviderServer) PricingPodPrice(ctx context.Context, req *protos.PricingPodPriceRequest) (*protos.PricingPodPriceResponse, error) {
+	//here TODO the real computations
+	return nil, status.Error(codes.Unimplemented, "function PricingPodPrice is Unimplemented")
+}
+
+// GPULabel returns the label added to nodes with GPU resource.
+func (c *cloudProviderServer) GPULabel(ctx context.Context, req *protos.GPULabelRequest) (*protos.GPULabelResponse, error) {
+	//here TODO the real computations
+	log.Printf("Requested label for gpu nodes")
+	return &protos.GPULabelResponse{
+		Label: "gpu=yes",
+	}, nil
+}
+
+// GetAvailableGPUTypes return all available GPU types cloud provider supports.
+func (c *cloudProviderServer) GetAvailableGPUTypes(ctx context.Context, req *protos.GetAvailableGPUTypesRequest) (*protos.GetAvailableGPUTypesResponse, error) {
+	//here TODO the real computations
+	log.Printf("Requested the available gpu types")
+	return &protos.GetAvailableGPUTypesResponse{
+		GpuTypes: map[string]*anypb.Any{},
+	}, nil
+}
+
+// Cleanup cleans up open resources before the cloud provider is destroyed, i.e. go routines etc.
+func (c *cloudProviderServer) Cleanup(ctx context.Context, req *protos.CleanupRequest) (*protos.CleanupResponse, error) {
+	//here TODO the real computations
+	return &protos.CleanupResponse{}, nil
+}
+
+// Refresh is called before every main loop and can be used to dynamically update cloud provider state.
+func (c *cloudProviderServer) Refresh(ctx context.Context, req *protos.RefreshRequest) (*protos.RefreshResponse, error) {
+	//here TODO the real computations
+	return &protos.RefreshResponse{}, nil
+}
+
+// NodeGroupTargetSize returns the current target size of the node group. It is possible
+// that the number of nodes in Kubernetes is different at the moment but should be equal
+// to the size of a node group once everything stabilizes (new nodes finish startup and
+// registration or removed nodes are deleted completely).
+func (c *cloudProviderServer) NodeGroupTargetSize(ctx context.Context, req *protos.NodeGroupTargetSizeRequest) (*protos.NodeGroupTargetSizeResponse, error) {
+	//here TODO the real computations
+	if test == 2 {
+		log.Printf("TARGET SIZE CURRENT OF  %s is 2", req.Id)
+		return &protos.NodeGroupTargetSizeResponse{
+			TargetSize: 2,
+		}, nil
+	} else {
+		log.Printf("TARGET SIZE CURRENT OF  %s is 1", req.Id)
+		return &protos.NodeGroupTargetSizeResponse{
+			TargetSize: 1,
+		}, nil
+	}
+}
+
+// NodeGroupIncreaseSize increases the size of the node group. To delete a node you need
+// to explicitly name it and use NodeGroupDeleteNodes. This function should wait until
+// node group size is updated.
+func (c *cloudProviderServer) NodeGroupIncreaseSize(ctx context.Context, req *protos.NodeGroupIncreaseSizeRequest) (*protos.NodeGroupIncreaseSizeResponse, error) {
+	//here TODO the real computations
+	if test == 1 {
+		test = 2
+		log.Printf("INCREASE SIZE of nodegroup %s", req.Id)
+		cmd := exec.Command(
+			"ssh",
+			"-J", "bastion@ssh.crownlabs.polito.it",
+			"crownlabs@10.97.97.14",
+			"liqoctl", "peer", "out-of-band", "remoto",
+			"--auth-url", "https://172.16.203.62:32473",
+			"--cluster-id", "1b5f548d-630b-4a95-90e2-9157b5a560ba",
+			"--auth-token", "dea56520895f222a8575f58270f08df46a8249d7180da6b5b747dd9cd2d62261e704a3c1c9b21abfdb0094eb02e2e5401776634e64f6aca480549c423fbca936",
+		)
+		output, err := cmd.CombinedOutput()
+		log.Printf("End SSH")
+		if err != nil {
+			log.Printf("Error during SSH: %v", err)
+			//return nil,err
+		}
+		log.Printf(" %s", output)
+	}
+	log.Printf("Increased size of nodegroup %s", req.Id)
+	return &protos.NodeGroupIncreaseSizeResponse{}, nil
+}
+
+// NodeGroupDeleteNodes deletes nodes from this node group (and also decreasing the size
+// of the node group with that). Error is returned either on failure or if the given node
+// doesn't belong to this node group. This function should wait until node group size is updated.
+func (c *cloudProviderServer) NodeGroupDeleteNodes(ctx context.Context, req *protos.NodeGroupDeleteNodesRequest) (*protos.NodeGroupDeleteNodesResponse, error) {
+	//here TODO the real computations
+	if test == 2 {
+		test = 1
+		log.Printf("DECREASE SIZE of %s NODEGROUP, DELETING NODE %s", req.Id, req.Nodes[0].Name)
+		log.Printf("list size is %d", len(req.Nodes))
+		for i, node := range req.Nodes {
+			log.Printf("Node %d: %s", i, node.Name)
+		}
+		log.Printf("Node fine loop")
+		cmd := exec.Command(
+			"ssh",
+			"-J", "bastion@ssh.crownlabs.polito.it",
+			"crownlabs@10.97.97.14",
+			"liqoctl", "unpeer", "remoto", "--skip-confirm",
+		)
+		output, err := cmd.CombinedOutput()
+		log.Printf("Fine SSH")
+		if err != nil {
+			log.Printf("Error during SSH: %v", err)
+			//return nil,err
+		}
+		log.Printf(" %s", output)
+	}
+	return &protos.NodeGroupDeleteNodesResponse{}, nil
+}
+
+// NodeGroupDecreaseTargetSize decreases the target size of the node group. This function
+// doesn't permit to delete any existing node and can be used only to reduce the request
+// for new nodes that have not been yet fulfilled. Delta should be negative. It is assumed
+// that cloud provider will not delete the existing nodes if the size when there is an option
+// to just decrease the target.
+func (c *cloudProviderServer) NodeGroupDecreaseTargetSize(ctx context.Context, req *protos.NodeGroupDecreaseTargetSizeRequest) (*protos.NodeGroupDecreaseTargetSizeResponse, error) {
+	//here TODO the real computations
+	log.Printf("decrease SIZE target why? of %s", req.Id)
+	return &protos.NodeGroupDecreaseTargetSizeResponse{}, nil
+}
+
+// NodeGroupNodes returns a list of all nodes that belong to this node group.
+func (c *cloudProviderServer) NodeGroupNodes(ctx context.Context, req *protos.NodeGroupNodesRequest) (*protos.NodeGroupNodesResponse, error) {
+	//here TODO the real computations
+	log.Printf("INFO ABOUT NODES OF NODEGROUPS, per nodegropu %s", req.Id)
+	if test == 1 {
+		log.Printf("return sud-> 1 node")
+		return &protos.NodeGroupNodesResponse{
+			Instances: []*protos.Instance{
+				{
+					Id: "instance-zf6d5",
+					Status: &protos.InstanceStatus{
+						InstanceState: 1,
+					},
+				},
+			},
+		}, nil
+	} else {
+		log.Printf("return sud-> 2 nodes")
+		return &protos.NodeGroupNodesResponse{
+			Instances: []*protos.Instance{
+				{
+					Id: "instance-zf6d5",
+					Status: &protos.InstanceStatus{
+						InstanceState: 1,
+					},
+				},
+				{
+					Id: "liqo-remoto",
+					Status: &protos.InstanceStatus{
+						InstanceState: 1,
+					},
+				},
+			},
+		}, nil
+	}
+}
+
+// NodeGroupTemplateNodeInfo returns a structure of an empty (as if just started) node,
+// with all of the labels, capacity and allocatable information. This will be used in
+// scale-up simulations to predict what would a new node look like if a node group was expanded.
+// Implementation optional: if unimplemented return error code 12 (for `Unimplemented`)
+func (c *cloudProviderServer) NodeGroupTemplateNodeInfo(ctx context.Context, req *protos.NodeGroupTemplateNodeInfoRequest) (*protos.NodeGroupTemplateNodeInfoResponse, error) {
+	log.Printf("INFO templateeeeeeeeeeee")
+	return nil, status.Error(codes.Unimplemented, "function NodeGroupTemplateNodeInfo is Unimplemented")
+}
+
+// GetOptions returns NodeGroupAutoscalingOptions that should be used for this particular
+// NodeGroup.
+// Implementation optional: if unimplemented return error code 12 (for `Unimplemented`)
+func (c *cloudProviderServer) NodeGroupGetOptions(ctx context.Context, req *protos.NodeGroupAutoscalingOptionsRequest) (*protos.NodeGroupAutoscalingOptionsResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "function NodeGroupGetOptions is Unimplemented")
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":9007")
+	if err != nil {
+		log.Fatalf("failed to start server, %v ", err)
+	}
+
+	server := grpc.NewServer()
+	service := &cloudProviderServer{}
+
+	protos.RegisterCloudProviderServer(server, service)
+	//require.NoError(t, err)
+
+	log.Printf("server partito")
+	server.Serve(lis)
+
+}
